cdce5a1c1323a1612d66581cdac441fdc8304d1f
K1NGDANO <big62coin@gmail.com>
2021-07-08 19:57:45 -1000

    changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
+
 import { Provider } from 'react-redux';
+
+import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
+
+import { store } from './app/store';
 
 ReactDOM.render(
   <React.StrictMode>
